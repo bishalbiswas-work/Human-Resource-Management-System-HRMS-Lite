3ef6e3412211e17bcc9f7a1c0f63fbc1c8272eb8
bishalbiswas-work <bishalbiswas.work@gmail.com>
2026-02-24 13:39:51 +0530

    modified the logic in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -10,21 +10,21 @@ const Sidebar = ({ currentWindow, setView }: SidebarProps) => {
                 <ul className="space-y-1">
                     <li
                         onClick={() => setView('dashboard')}
-                        className={`px-4 py-2 rounded-md shadow-sm text-sm font-medium cursor-pointer transition-all ${currentView === 'dashboard' ? 'bg-white text-blue-600 border border-gray-100' : 'text-gray-600 hover:bg-gray-100'
+                        className={`px-4 py-2 rounded-md shadow-sm text-sm font-medium cursor-pointer transition-all ${currentWindow === 'dashboard' ? 'bg-white text-blue-600 border border-gray-100' : 'text-gray-600 hover:bg-gray-100'
                             }`}
                     >
                         Dashboard
                     </li>
                     <li
                         onClick={() => setView('employees')}
-                        className={`px-4 py-2 text-sm font-medium rounded-md cursor-pointer transition-all ${currentView === 'employees' ? 'bg-white text-blue-600 border border-gray-100' : 'text-gray-600 hover:bg-gray-100'
+                        className={`px-4 py-2 text-sm font-medium rounded-md cursor-pointer transition-all ${currentWindow === 'employees' ? 'bg-white text-blue-600 border border-gray-100' : 'text-gray-600 hover:bg-gray-100'
                             }`}
                     >
                         Employees
                     </li>
                     <li
                         onClick={() => setView('attendance')}
-                        className={`px-4 py-2 text-sm font-medium rounded-md cursor-pointer transition-all ${currentView === 'attendance' ? 'bg-white text-blue-600 border border-gray-100' : 'text-gray-600 hover:bg-gray-100'
+                        className={`px-4 py-2 text-sm font-medium rounded-md cursor-pointer transition-all ${currentWindow === 'attendance' ? 'bg-white text-blue-600 border border-gray-100' : 'text-gray-600 hover:bg-gray-100'
                             }`}
                     >
                         Attendance
