66d353608f840e7ed9d57a124fa24164f8c6628f
bishalbiswas-work <bishalbiswas.work@gmail.com>
2026-02-24 12:52:41 +0530

    imported modules on dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,6 +1,15 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Overview from "./Overview";
+import EmployeeManagement from "./EmployeeManagement";
+import AttendanceManagement from "./AttendanceManagement";
+
+
 const Dashboard = () => {
     return (
         <div>
+            <Navbar />
+            <Sidebar />
             <h1>Dashboard</h1>
         </div>
     )
