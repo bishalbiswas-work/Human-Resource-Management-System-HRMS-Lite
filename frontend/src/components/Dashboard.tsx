2a4f0cf3b6d9d53aa3bdc325bf310fe5393f7824
bishalbiswas-work <bishalbiswas.work@gmail.com>
2026-02-24 13:02:11 +0530

    added the logic on dashboard compoenent

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Overview from "./Overview";
@@ -6,11 +7,29 @@ import AttendanceManagement from "./AttendanceManagement";
 
 
 const Dashboard = () => {
+    const [currentWindow, setCurrentWindow] = useState("overview");
+    const renderView = () => {
+        switch (currentWindow) {
+            case "overview":
+                return <Overview />;
+            case "employees":
+                return <EmployeeManagement />;
+            case "attendance":
+                return <AttendanceManagement />;
+            default:
+                return <Overview />;
+        }
+    }
     return (
-        <div>
+        <div className="flex flex-col h-screen overflow-hidden">
             <Navbar />
-            <Sidebar />
-            <h1>Dashboard</h1>
+            <div className="flex flex-1 overflow-hidden">
+                <Sidebar currentWindow={currentWindow} setView={setCurrentWindow} />
+                <div className="flex-1 overflow-y-auto p-6">
+                    {renderView()}
+                </div>
+            </div>
+
         </div>
     )
 }
